ceb4e23be8af2a0b7b19d79d07d6d5a1548e315b
idn-fahmifn <fahmifn.idn@gmail.com>
2025-10-14 11:20:55 +0700

    2.4

--- app/Http/Controllers/UmurController.php
+++ app/Http/Controllers/UmurController.php
@@ -13,7 +13,25 @@ class UmurController extends Controller
 
     public function proses(Request $request)
     {
-        return $request;
+
+        $request->validate([
+            'name' => ['string', 'min:3', 'max:20', 'required'],
+            'age' => ['numeric', 'min:1', 'max:90', 'required'],
+        ], [
+
+            'name.string' => 'Salah om, harus string',
+            'name.min' => 'Salah om, minimal 3 karakter',
+            'name.max' => 'Salah om, maksimal 20 karakter',
+            'name.required' => 'Salah om, wajib diisi',
+
+            'age.numeric' => 'Salah om, harus angka',
+            'age.min' => 'Salah om, minimal 1 umurnya',
+            'age.max' => 'Salah om, maksimal 90 umurnya',
+            'age.required' => 'Salah om, wajib diisi',
+
+        ]);
+
+        return redirect()->route('umur.sukses');
     }
 
     public function sukses()
--- resources/views/umur/form.blade.php
+++ resources/views/umur/form.blade.php
@@ -1,16 +1,33 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Umur</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
     <div class="container mt-4">
         <div class="card p-4">
             <div class="card-body">
                 <div class="card-title">Masukan umur kamu dibawah :</div>
+
+                @if ($errors->any())
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Wah ada error!</strong>
+                        <ol>
+                            @foreach ($errors->all() as $row)
+                            <li>{{ $row }}</li>
+                            @endforeach
+                        </ol>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
+
 
                 <div class="mt-4">
                     <form action="{{ Route('umur.proses') }}" method="post">
@@ -35,6 +52,9 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
